d75274281ce3374d44c26619c2c798c1a419dfef
Eduard Ciugulea <ciugulea@yahoo.com>
2021-06-03 15:47:29 +0100

    change in controller courts

--- app/models/court.rb
+++ app/models/court.rb
@@ -1,6 +1,6 @@
 class Court < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   validates :address, presence: true
   validates :price, presence: true
