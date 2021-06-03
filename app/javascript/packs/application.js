edf021ec993b0670d743921ede6f7a5d2a112795
Eduard Ciugulea <ciugulea@yahoo.com>
2021-06-03 17:08:22 +0100

    new

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -40,3 +40,16 @@ import { initMapbox } from '../plugins/init_mapbox';
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
 })
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navy");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
