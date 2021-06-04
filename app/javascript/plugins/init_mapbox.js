3750da17c4ad6ffd82cb3f0cafdb398c4be672ce
Eduard Ciugulea <ciugulea@yahoo.com>
2021-06-04 16:07:10 +0100

    marker removed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -15,7 +15,6 @@ const addMarkersToMap = (map, markers) => {
 
     const element = document.createElement('div');
     element.className = 'marker';
-    element.style.backgroundImage = `url('${marker.image_url}')`;
     element.style.backgroundSize = 'contain';
     element.style.width = '50px';
     element.style.height = '50px';
